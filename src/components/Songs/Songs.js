993e9cb4ba4e6081380fca9c9796206ae9236003
Keith <keith.githinji@student.moringaschool.com>
2024-11-12 14:19:02 +0300

    added components

---
+++ src/components/Songs/Songs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Songs.module.css';
+
+const Songs = () => (
+  <div className={styles.Songs}>
+    Songs Component
+  </div>
+);
+
+Songs.propTypes = {};
+
+Songs.defaultProps = {};
+
+export default Songs;
