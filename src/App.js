865c5f51327f0ddb36e2c713bc705b6eaef3a384
Leguminous <murayaleo911@gmail.com>
2024-11-14 17:21:12 +0300

    finally done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Homepage from './components/Homepage/Homepage';
+import Artists from './components/Artists/Artists';
+import Songs from './components/Songs/Songs';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+       
+        <NavBar />
+
+        
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/artists" element={<Artists />} />
+          <Route path="/songs" element={<Songs />} />
+         
+          <Route path="*" element={<Homepage />} />
+        </Routes>
+
+       
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
